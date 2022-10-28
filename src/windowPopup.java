150b45ce644cccd76faf789f8b51f2bbd05f2910
deepuforever14 <deepuforever92@gmail.com>
2022-10-28 16:28:43 +0530

    American Team

--- src/windowPopup.java
+++ src/windowPopup.java
@@ -7,14 +7,15 @@ public class windowPopup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//American
-		//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+	//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
 		//WebDriver driver = new ChromeDriver();
 		  System.setProperty("webdriver.gecko.driver","D:\\geckodriver-v0.31.0-win32\\geckodriver.exe");
  	  WebDriver driver = new FirefoxDriver();
 		//driver.get("https://the-internet.herokuapp.com/");
 		driver.get("https://admin:admin@the-internet.herokuapp.com");
 		driver.findElement(By.linkText("Basic Auth")).click();
+		System.out.println(driver.getTitle());
+		driver.close();
 	}
 
 }
