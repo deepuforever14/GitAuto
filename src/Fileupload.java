fa36c62a110bd3e970c778bd14e4a8909826a7c8
deepuforever14 <deepuforever92@gmail.com>
2022-10-28 16:18:06 +0530

    American Developer

--- src/Fileupload.java
+++ src/Fileupload.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Fileupload {
-
+//American
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		//HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
--- src/windowPopup.java
+++ src/windowPopup.java
@@ -7,6 +7,7 @@ public class windowPopup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//American
 		//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
 		//WebDriver driver = new ChromeDriver();
 		  System.setProperty("webdriver.gecko.driver","D:\\geckodriver-v0.31.0-win32\\geckodriver.exe");
