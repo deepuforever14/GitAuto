730751136d799a0d1f85221c34661ae68d16aea9
deepuforever14 <deepuforever92@gmail.com>
2022-10-28 16:14:25 +0530

    Modified by Indian Developer

--- src/windowPopup.java
+++ src/windowPopup.java
@@ -7,6 +7,7 @@ public class windowPopup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Indian
 		//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
 		//WebDriver driver = new ChromeDriver();
 		  System.setProperty("webdriver.gecko.driver","D:\\geckodriver-v0.31.0-win32\\geckodriver.exe");
@@ -14,6 +15,8 @@ public class windowPopup {
 		//driver.get("https://the-internet.herokuapp.com/");
 		driver.get("https://admin:admin@the-internet.herokuapp.com");
 		driver.findElement(By.linkText("Basic Auth")).click();
+		System.out.println(driver.getTitle());
+		driver.close();
 	}
 
 }
